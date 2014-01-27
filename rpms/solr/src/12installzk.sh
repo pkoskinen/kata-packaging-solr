6d9916e783635e1e55c91d18a824e85c3ab59216
Pinja Koskinen <none@example.org>
2014-01-27 08:52:13 +0200

    The zookeeper-server init command removes the myid file, so init now executes after creating the file

--- rpms/solr/src/12installzk.sh
+++ rpms/solr/src/12installzk.sh
@@ -3,6 +3,8 @@ set -x
 patchdir=$1
 datadir=$2
 
+service zookeeper-server init
+
 echo 1 > /var/lib/zookeeper/myid
 
 pushd /var/lib/zookeeper/ >/dev/null
@@ -15,7 +17,6 @@ patch -b -p5 -i "${patchdir}/zoo.cfg.patch"
 /usr/bin/python /usr/share/mcfg/tool/mcfg.py run /usr/share/mcfg/config/kata-template.ini /root/kata-master.ini 13
 popd >/dev/null
 
-service zookeeper-server init
 chown -R zookeeper:zookeeper /var/lib/zookeeper/
 
 chkconfig zookeeper-server on
